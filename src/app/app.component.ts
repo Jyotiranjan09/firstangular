be0173a689a1db855f3d04d2f6cba962aaf4e861
Jyotiranjan09 <jyotiranjan73234@gmail.com>
2025-12-05 15:21:04 +0530

    directive changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,41 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // selector: '.app-root',
+  // selector: '[app-root]',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // intrerpolation
   title = 'firstangular';
+
+
+  // property binding
+  imageurl='assets/image.pnghhjhbjjjjjjjjjjjjjjjjjjjjj'
+
+
+  //event binding
+    showMessage(){
+    alert('Button clicked!');
+  }
+
+  //two way data binding
+  name:any="";
+
+  // Structural Directives
+  // *ngIf
+  message=true;
+
+  //*ngFor
+  fruits=["Apple","Banana","Mango"];
+
+  //*ngSwitch
+  day="Monday";
+
+  //attribute directive
+  isHighlighted=true;
+  textColor='blue';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
